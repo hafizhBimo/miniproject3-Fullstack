24d549282a9059b1540e98c4f657044b4e206a91
hafizhbimo <hfzhbimo@gmail.com>
2023-07-01 20:46:56 +0700

    add controller for create new category

--- projects/server/src/controller/category.js
+++ projects/server/src/controller/category.js
@@ -17,4 +17,30 @@ module.exports = {
       });
     }
   },
+
+  async addCategory(req, res) {
+    const { newCategory } = req.body;
+    try {
+      const addCategory = await db.Category.findOrCreate({
+        where: { name: newCategory },
+        defaults: {
+          name: newCategory,
+        },
+      });
+      if (addCategory) {
+        return res.status(400).send({
+          message: "this category's name already exist",
+        });
+      }
+      return res.status(200).send({
+        message: "new category successfully added",
+        data: addCategory,
+      });
+    } catch (errors) {
+      res.status(500).send({
+        message: "something wrong on the server",
+        error: errors.message,
+      });
+    }
+  },
 };
--- projects/server/src/routes/category.js
+++ projects/server/src/routes/category.js
@@ -2,5 +2,6 @@ const { category: authCategory } = require("./../controller");
 const router = require("express").Router();
 
 router.get("/categories", authCategory.getAllCategories);
+router.post("/new-category", authCategory.addCategory);
 
 module.exports = router;
