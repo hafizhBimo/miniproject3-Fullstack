d6e237559832d8682e8e14874a3852f7b50003d7
hafizhbimo <hfzhbimo@gmail.com>
2023-07-04 21:39:38 +0700

    add currency for product listing

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -56,8 +56,6 @@ const router = createBrowserRouter([
       },
     ],
   },
-  ,
-  ,
 ]);
 
 function App() {
--- projects/client/src/component/Product/index.js
+++ projects/client/src/component/Product/index.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Card, Badge } from "flowbite-react";
+import rupiah from "../../utils/currency";
 
 const Product = () => {
   const [userData, setUserData] = useState([]);
   const [totalPage, setTotalPages] = useState([]);
+
   useEffect(() => {
     axios
       .get("http://localhost:8000/api/product")
@@ -36,8 +38,8 @@ const Product = () => {
           </a>
           <h6>{Product.User.username}</h6>
           <div className="flex items-center justify-between">
-            <span className="text-3xl font-bold text-gray-900 dark:text-white">
-              Rp.{Product.price}
+            <span className="text-2xl font-bold text-gray-900 dark:text-white">
+              {rupiah(Product.price)}
             </span>
             <a
               className="rounded-lg bg-cyan-700 px-1 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
