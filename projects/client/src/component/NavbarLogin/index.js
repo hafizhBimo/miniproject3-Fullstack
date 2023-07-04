c74aa156aa1467bfa3d2f877fcbb52d9b89ab460
hafizhbimo <hfzhbimo@gmail.com>
2023-07-04 20:25:07 +0700

    styling-FE

---
+++ projects/client/src/component/NavbarLogin/index.js
@@ -0,0 +1,55 @@
+import { Button, Dropdown, Navbar, Avatar } from "flowbite-react";
+import { useNavigate, Link } from "react-router-dom";
+
+const NavbarLogin = () => {
+  const navigate = useNavigate();
+  return (
+    <Navbar fluid rounded>
+      <Navbar.Brand href="/">
+        <img
+          alt="Flowbite React Logo"
+          className="mr-3 h-6 sm:h-9"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxdkHKLUaOFD-PuO8f9lOb-nRTaphpfXxQqyCaWPRsFN8HXZK7Ja0g6shAJkh6sYITTuM&usqp=CAU"
+        />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          eat-sleep-game-repeat
+        </span>
+      </Navbar.Brand>
+      <div className="flex md:order-2">
+        <Dropdown
+          arrowIcon={false}
+          inline={true}
+          label={
+            <Avatar
+              alt="User settings"
+              img="/Avatar-PNG-Photos.png"
+              rounded={true}
+            />
+          }
+        >
+          <Dropdown.Header>
+            <span className="block text-sm">neytiri</span>
+          </Dropdown.Header>
+          <Dropdown.Item>
+            <Link to="/myStore">My Store</Link>
+          </Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item>
+            <a href="/" onClick={() => localStorage.removeItem("token")}>
+              Sign Out
+            </a>
+          </Dropdown.Item>
+        </Dropdown>
+
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link href="/">Home</Navbar.Link>
+        <Navbar.Link href="/CreateProductListing">Create Product</Navbar.Link>
+        <Navbar.Link href="/ManageCategory">Manage Category</Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default NavbarLogin;
--- projects/client/src/component/Product/index.js
+++ projects/client/src/component/Product/index.js
@@ -1,40 +1,52 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Card, Badge } from "flowbite-react";
 
 const Product = () => {
-  const [userData, setUserData]=useState([])
-  const [totalPage, setTotalPages]=useState([])
+  const [userData, setUserData] = useState([]);
+  const [totalPage, setTotalPages] = useState([]);
   useEffect(() => {
     axios
       .get("http://localhost:8000/api/product")
       .then((response) => {
-        console.log(response,"ini siapa");
         setUserData(response.data.data);
         setTotalPages(Math.ceil(response.data.rows / 9));
+        console.log(response, "ini userdata");
       })
       .catch((err) => console.log(err));
   }, []);
 
   return (
-    <div>
+    <div className="grid grid-cols-4 gap-3">
       {userData.map((Product) => (
-        <div key={Product.id}>
-          <div className="w-full relative group">
-            <div className="max-w-80 max-h-80 relative overflow-y-hidden"></div>
-            <img
-              src={`http://localhost:8000${Product.imageUrl}`}
-              alt=""
-            />
+        <Card
+          key={Product.id}
+          imgAlt="test"
+          imgSrc={`http://localhost:8000${Product.imageUrl}`}
+        >
+          <div className="">
+            <span class="bg-indigo-100 text-indigo-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-indigo-900 dark:text-indigo-300">
+              {Product.Category.name}
+            </span>
           </div>
-          <div className="max-w-80 py-6 flex flex-col gap-1 border-[1px] border-t-0 px-4">
-            <div className="flex items-center justify-between font-titleFont">
-              <h2 className="text-lg text-primeColor font-bold">
-                {Product.name}
-              </h2>
-              <p className="text-[#767676] text-[14px]">${Product.price}</p>
-            </div>
+          <a href="#">
+            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+              <p>{Product.name}</p>
+            </h5>
+          </a>
+          <h6>{Product.User.username}</h6>
+          <div className="flex items-center justify-between">
+            <span className="text-3xl font-bold text-gray-900 dark:text-white">
+              Rp.{Product.price}
+            </span>
+            <a
+              className="rounded-lg bg-cyan-700 px-1 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
+              href="#"
+            >
+              <p>Add to cart</p>
+            </a>
           </div>
-        </div>
+        </Card>
       ))}
     </div>
   );
