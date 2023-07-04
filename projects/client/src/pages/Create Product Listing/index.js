f8407497f2f4b11980cadc4eec333ce7c274d8c6
ncdncd <arka.socky@gmail.com>
2023-07-04 11:29:36 +0700

    category dropdown default

--- projects/client/src/pages/Create Product Listing/index.js
+++ projects/client/src/pages/Create Product Listing/index.js
@@ -9,7 +9,7 @@ const CreateProductListing = () => {
 
   const [value, setValue] = useState("");
   const [categoryData, setCategoryData] = useState([]);
-  const [selectedItem, setSelectedItem] = useState('');
+  const [selectedItem, setSelectedItem] = useState('1');
 
   const navigate = useNavigate();
 
