f6406d361500055841ba21ed4d05d932ac1bbbc1
ncdncd <arka.socky@gmail.com>
2023-07-05 15:14:32 +0700

    add to cart FE

--- projects/client/src/pages/ProductDetail/index.js
+++ projects/client/src/pages/ProductDetail/index.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 const ProductDetail = () => {
+
+  const navigate = useNavigate();
   const [userData, setUserData] = useState([]);
   const [cartData, setCartData] = useState([]);
+  const [quantityData, setQuantityData] = useState(1)
   const { id } = useParams();
 
   useEffect(() => {
@@ -26,15 +29,24 @@ const ProductDetail = () => {
       axios.post(
         `http://localhost:8000/api/product/${productId}`,
         {
+          quantity: quantityData
+        },
+        {
           headers: {Authorization: `Bearer ${token}`},
         },
 
       ).then((response) => {
           setCartData(response);
-        });
+        })
+        // .catch((err) => {
+        //   if (err.message === "Request failed with status code 401") {
+        //     navigate("/login");
+        //   }
+        // });
     }
 
   }
+
 
   return (
     <div>
@@ -61,6 +73,15 @@ const ProductDetail = () => {
                   <button onClick={() => handleClick(Product.id)} className=" bg-black py-4 px-8 text-white">
                     Add to cart
                   </button>
+                  <div>
+                    <button onClick={() => setQuantityData(quantityData - 1)} className=" bg-blue-500 py-2 px-4 text-white">
+                      -
+                    </button>
+                    <div> {quantityData} </div>
+                    <button onClick={() => setQuantityData(quantityData + 1)} className=" bg-blue-500 py-2 px-4 text-white">
+                      +
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
