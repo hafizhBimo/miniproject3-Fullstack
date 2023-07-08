6d47ee3ff4971995e1e0c6a6a38966be956754be
ncdncd <arka.socky@gmail.com>
2023-07-08 12:55:20 +0700

    total transaction backend

--- projects/server/src/models/order_items.js
+++ projects/server/src/models/order_items.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Order_items.belongsTo(models.Order_details, { foreignKey: "order_id" });
     }
   }
   Order_items.init({
