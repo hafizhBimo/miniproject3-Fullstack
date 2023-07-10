9490953c7e8ab4779bb059134db82cfd8dae053b
hafizhbimo <hfzhbimo@gmail.com>
2023-07-11 00:58:22 +0700

    create product alert final

--- projects/client/src/pages/Create Product Listing/index.js
+++ projects/client/src/pages/Create Product Listing/index.js
@@ -8,17 +8,18 @@ import {
   FileInput,
   Button,
   Dropdown,
+  Alert,
 } from "flowbite-react";
 import { Link, useNavigate } from "react-router-dom";
 import withAuth from "../../component/withAuth";
+import { HiInformationCircle } from "react-icons/hi";
 
 const CreateProductListing = () => {
   const [value, setValue] = useState("");
   const [categoryData, setCategoryData] = useState([]);
+  const [alertMessage, setAlertMessage] = useState("");
 
   const [selectedItem, setSelectedItem] = useState("1");
-
- 
 
   const navigate = useNavigate();
 
@@ -55,6 +56,7 @@ const CreateProductListing = () => {
       .then((response) => {
         console.log(response);
         setValue(response.data);
+        setAlertMessage(response.data.message);
       })
       .catch((err) => console.log(err));
 
@@ -64,97 +66,118 @@ const CreateProductListing = () => {
   };
 
   return (
-    <Formik
-      initialValues={{
-        file: null,
-        name: "",
-        description: "",
-        price: "",
-        categoryId: "",
-      }}
-      // validationSchema={CreateSchema}
-      onSubmit={handleSubmit}
-    >
-      {(props) => (
-        <form
-          className="flex flex-col gap-4 items-center"
-          onSubmit={props.handleSubmit}
+    <>
+      {alertMessage ? (
+        <Alert
+          color="success"
+          icon={HiInformationCircle}
+          onDismiss={() => setAlertMessage("")}
         >
-          <div className="flex flex-col" style={{ width: "600px" }}>
-            <div className="mb-2 block">
-              <Label htmlFor="name" value="Product Name" />
-              <TextInput
-                id="name"
-                type="text"
-                placeholder="name of product"
-                name="name"
-                onChange={props.handleChange}
-                value={props.values.name}
-              />
-              <Label htmlFor="price" value="Price" />
-              <TextInput
-                type="text"
-                placeholder="price of product"
-                name="price"
-                onChange={props.handleChange}
-                value={props.values.price}
-              />
+          <span>
+            <p>
+              <span className="font-medium">{alertMessage}</span>
+            </p>
+          </span>
+        </Alert>
+      ) : (
+        alertMessage
+      )}
+      <Formik
+        initialValues={{
+          file: null,
+          name: "",
+          description: "",
+          price: "",
+          categoryId: "",
+        }}
+        // validationSchema={CreateSchema}
+        onSubmit={handleSubmit}
+      >
+        {(props) => (
+          <form
+            className="flex flex-col gap-4 items-center"
+            onSubmit={props.handleSubmit}
+          >
+            <div className="flex flex-col" style={{ width: "600px" }}>
+              <div className="mb-2 block">
+                <Label htmlFor="name" value="Product Name" />
+                <TextInput
+                  id="name"
+                  type="text"
+                  placeholder="name of product"
+                  name="name"
+                  onChange={props.handleChange}
+                  value={props.values.name}
+                  required
+                />
+                <Label htmlFor="price" value="Price" />
+                <TextInput
+                  type="text"
+                  placeholder="price of product"
+                  name="price"
+                  onChange={props.handleChange}
+                  value={props.values.price}
+                  required
+                />
 
-              <div id="textarea">
-                <div className="mb-2 block">
-                  <Label htmlFor="description" value="Description" />
-                  <Textarea
-                    type="text"
-                    placeholder="description of product"
-                    name="description"
-                    onChange={(event) => {
-                      props.setFieldValue("description", event.target.value);
-                    }}
-                  />
-                </div>
-              </div>
-            </div>
-            <div>
-              <div>
-                <div className="mb-2 block">
-                  <Label
-                    htmlFor="file"
-                    value="Upload picture of product here"
-                  />
-                  <FileInput
-                    type="file"
-                    name="file"
-                    id="file"
-                    className="file"
-                    htmlFor="file"
-                    onChange={(e) => {
-                      props.setFieldValue("file", e.currentTarget.files[0]);
-                    }}
-                  />
+                <div id="textarea">
+                  <div className="mb-2 block">
+                    <Label htmlFor="description" value="Description" />
+                    <Textarea
+                      type="text"
+                      placeholder="description of product"
+                      name="description"
+                      onChange={(event) => {
+                        props.setFieldValue("description", event.target.value);
+                      }}
+                      required
+                    />
+                  </div>
                 </div>
               </div>
               <div>
                 <div>
-                  <select value={selectedItem} onChange={handleSelectChange}>
-                    {categoryData.map((cat) => (
-                      <option key={cat.id} value={cat.id}>
-                        {cat.name}
-                      </option>
-                    ))}
-                  </select>
+                  <div className="mb-2 block">
+                    <Label
+                      htmlFor="file"
+                      value="Upload picture of product here"
+                    />
+                    <FileInput
+                      type="file"
+                      name="file"
+                      id="file"
+                      className="file"
+                      htmlFor="file"
+                      onChange={(e) => {
+                        props.setFieldValue("file", e.currentTarget.files[0]);
+                      }}
+                      required
+                    />
+                  </div>
+                </div>
+                <div>
                   <div>
-                    <Button size="lg" type="submit">
-                      Post
-                    </Button>
+                    <select value={selectedItem} onChange={handleSelectChange}>
+                      {categoryData.map((cat) => (
+                        <option key={cat.id} value={cat.id}>
+                          {cat.name}
+                        </option>
+                      ))}
+                    </select>
+                    <div>
+                      <Button size="lg" type="submit">
+                        Post
+                      </Button>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          {/* {JSON.stringify(props.values)} */}
-        </form>
-      )}
-    </Formik>
+            {/* {JSON.stringify(props.values)} */}
+          </form>
+        )}
+      </Formik>
+    </>
   );
 };
 
